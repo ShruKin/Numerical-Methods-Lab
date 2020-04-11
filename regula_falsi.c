c737ba81e2cfb24ca94e7c6bf4e5932b000db38f
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2020-04-11 17:15:47 +0530

    Added Regula Falsi Method

---
+++ regula_falsi.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include <math.h>
+#define E 0.00001
+
+double f(double x){
+    return pow(x,3) - x - 4;
+}
+
+int main(int argc, char const *argv[])
+{
+    double a, b, x0, x1, x2;
+    int n=0;
+
+    printf("Enter interval: ");
+    scanf("%lf%lf", &a, &b);
+
+    printf("\nn\tx0\t\tx1\t\tx2\t\tf(x2)\t\tf(x0)\t\tf(x1)\n");
+    for(int i=0; i<26; i++)
+        printf("----");
+    printf("\n");
+
+    x0 = a;
+    x1 = b;
+    do{
+        x2 = x0 - ((f(x0)*(x1-x0))/(f(x1)-f(x0)));
+        printf("%d\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\n", n++, x0, x1, x2, f(x2), f(x0), f(x1));
+
+        if((f(x0)*f(x2)) < 0.0)
+            x1 = x2;
+        else
+            x0 = x2; 
+    }while(fabs(f(x2)) >= E);
+
+    for(int i=0; i<26; i++)
+        printf("----");
+    printf("\n");
+
+    printf("The root of the equation is: %lf\n", x2);
+
+    return 0;
+}
